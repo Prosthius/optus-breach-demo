faa975567d12260f50daffbc713668509f01e68e
Prosthius <ietvntpd@duck.com>
2022-12-13 15:30:06 +1100

    Created new migration to rename table to 'users'

---
+++ api/Migrations/20221213035915_ModelRevisionTwo.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelRevisionTwo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserDatas",
+                table: "UserDatas");
+
+            migrationBuilder.RenameTable(
+                name: "UserDatas",
+                newName: "users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_users",
+                table: "users",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_users",
+                table: "users");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "UserDatas");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserDatas",
+                table: "UserDatas",
+                column: "Id");
+        }
+    }
+}
